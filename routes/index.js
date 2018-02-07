3ac19c594c975515c0e45ec1780ba7fb61a9702b
Jigyasaa Sood <jsood@ucsd.edu>
2018-02-07 02:15:54 -0800

    halfway done with lab, stuck on slide 19

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,14 @@
 // Get all of our friend data
+//so, all the fake "database" data file is loaded into the variable data
+//and this will get printed to the console via console.log whenever you 
+//load the page
+//STILL HAVE TO WORK ON THE LAB! STUCK ON SLIDE 19, GO TO LAB ON THURSDAY!!
+
 var data = require('../data.json');
 
 exports.view = function(req, res){
 	console.log(data);
-	res.render('index');
+	//res.render('index');
+	//render inserts variables into the HTML template
+	res.render('index','data')
 };
